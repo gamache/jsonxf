f2b77210aa0bc191b6506e7140ea58a2752dda6c
pete gamache <pete@gamache.org>
2018-01-14 15:07:48 -0500

    refactor almost done

--- src/jsonxf.rs
+++ src/jsonxf.rs
@@ -20,166 +20,265 @@ use std::io::Error;
 
 const BUF_SIZE: usize = 1024 * 16;
 
-const C_CR:             u8 = '\r' as u8;
-const C_LF:             u8 = '\n' as u8;
-const C_TAB:            u8 = '\t' as u8;
-const C_SPACE:          u8 = ' ' as u8;
+const C_CR: u8 = '\r' as u8;
+const C_LF: u8 = '\n' as u8;
+const C_TAB: u8 = '\t' as u8;
+const C_SPACE: u8 = ' ' as u8;
 
-const C_COMMA:          u8 = ',' as u8;
-const C_COLON:          u8 = ':' as u8;
-const C_QUOTE:          u8 = '"' as u8;
-const C_BACKSLASH:      u8 = '\\' as u8;
+const C_COMMA: u8 = ',' as u8;
+const C_COLON: u8 = ':' as u8;
+const C_QUOTE: u8 = '"' as u8;
+const C_BACKSLASH: u8 = '\\' as u8;
 
-const C_LEFT_BRACE:     u8 = '{' as u8;
-const C_LEFT_BRACKET:   u8 = '[' as u8;
-const C_RIGHT_BRACE:    u8 = '}' as u8;
-const C_RIGHT_BRACKET:  u8 = ']' as u8;
+const C_LEFT_BRACE: u8 = '{' as u8;
+const C_LEFT_BRACKET: u8 = '[' as u8;
+const C_RIGHT_BRACE: u8 = '}' as u8;
+const C_RIGHT_BRACKET: u8 = ']' as u8;
 
-struct Formatter {
-  // constants
-  indent: String,
-  line_separator: String,
-  record_separator: String,
-  after_colon: String,
+/// `Formatter` allows pretty-printing, minimizing, and other
+/// formatting tasks on JSON-encoded data in UTF-8 format.
+///
+/// Example:
+///
+/// ```
+/// let mut fmt = jsonxf::Formatter::pretty_printer();
+/// fmt.line_separator = String::from("\r\n");
+/// assert_eq!(
+///     fmt.format("{\"a\":1}").unwrap(),
+///     "{\r\n  \"a\": 1\r\n}"
+/// );
+/// ```
+pub struct Formatter {
+    /// Used for beginning-of-line indentation in arrays and objects.
+    pub indent: String,
 
-  // mutable state
-  depth: usize,        // current nesting depth
-  in_string: bool,     // is the next byte part of a string?
-  in_backslash: bool,  // does the next byte follow a backslash in a string?
-  empty: bool,         // is the next byte in an empty object or array?
-  first: bool,         // is this the first byte of input?
+    /// Used inside arrays and objects.
+    pub line_separator: String,
+
+    /// Used between root-level arrays and objects.
+    pub record_separator: String,
+
+    /// Used after a colon inside objects.
+    pub after_colon: String,
+
+    /// Used at very end of output.
+    pub trailing_output: String,
+
+    // private mutable state
+    depth: usize, // current nesting depth
+    in_string: bool, // is the next byte part of a string?
+    in_backslash: bool, // does the next byte follow a backslash in a string?
+    empty: bool, // is the next byte in an empty object or array?
+    first: bool, // is this the first byte of input?
 }
 
 impl Formatter {
-  fn default() -> Formatter {
-    Formatter {
-      indent: String::from("  "),
-      line_separator: String::from("\n"),
-      record_separator: String::from("\n"),
-      after_colon: String::from(" "),
-      depth: 0,
-      in_string: false,
-      in_backslash: false,
-      empty: false,
-      first: true,
+    fn default() -> Formatter {
+        Formatter {
+            indent: String::from("  "),
+            line_separator: String::from("\n"),
+            record_separator: String::from("\n"),
+            after_colon: String::from(" "),
+            trailing_output: String::from(""),
+            depth: 0,
+            in_string: false,
+            in_backslash: false,
+            empty: false,
+            first: true,
+        }
     }
-  }
 
-  pub fn pretty_printer() -> Formatter {
-    return Formatter::default();
-  }
-
-  pub fn minimizer() -> Formatter {
-    let mut xf = Formatter::default();
-    xf.indent = String::from("");
-    xf.line_separator = String::from("");
-    xf.record_separator = String::from("\n");
-    xf.after_colon = String::from("");
-    return xf;
-  }
-
-  pub fn format(&mut self, json_string: &str) -> Result<String, String> {
-    let mut input = json_string.as_bytes();
-    let mut output: Vec<u8> = vec![];
-    match self.format_stream(&mut input, &mut output) {
-      Ok(_) => {},
-      Err(f) => { return Err(f.to_string()); },
-    };
-    let output_string = match String::from_utf8(output) {
-      Ok(s) => { s },
-      Err(f) => { return Err(f.to_string()); },
-    };
-    return Ok(output_string);
-  }
-
-  pub fn format_stream(&mut self, input: &mut Read, output: &mut Write) -> Result<(), Error> {
-    let mut reader = BufReader::new(input);
-    let mut writer = BufWriter::new(output);
-    let mut buf = [0 as u8; BUF_SIZE];
-    loop {
-      match reader.read(&mut buf) {
-        Ok(0) => { break; },
-        Ok(n) => { self.format_buf(&buf[0..n], &mut writer)?; },
-        Err(e) => { return Err(e); },
-      }
+    /// Returns a Formatter set up for pretty-printing.
+    /// Defaults to using two spaces of indentation,
+    /// Unix newlines, and no whitespace at EOF.
+    ///
+    /// Example:
+    ///
+    /// ```
+    /// let mut pp = jsonxf::Formatter::pretty_printer();
+    /// assert_eq!(
+    ///     pp.format("{\"a\":1}").unwrap(),
+    ///     "{\n  \"a\": 1\n}"
+    /// );
+    /// ```
+    pub fn pretty_printer() -> Formatter {
+        return Formatter::default();
     }
-    return Ok(());
-  }
 
+    /// Returns a Formatter set up for minimizing.
+    /// Defaults to using Unix newlines between records,
+    /// and no whitespace at EOF.
+    ///
+    /// Example:
+    ///
+    /// ```
+    /// let mut min = jsonxf::Formatter::minimizer();
+    /// assert_eq!(
+    ///     min.format("{  \"a\" : 1  }\n").unwrap(),
+    ///     "{\"a\":1}"
+    /// );
+    /// ```
+    pub fn minimizer() -> Formatter {
+        let mut xf = Formatter::default();
+        xf.indent = String::from("");
+        xf.line_separator = String::from("");
+        xf.record_separator = String::from("\n");
+        xf.after_colon = String::from("");
+        return xf;
+    }
 
-  /* Formats the contents of `buf` into `writer`. */
-  fn format_buf(&mut self, buf: &[u8], writer: &mut Write) -> Result<(),Error> {
-    for n in 0..buf.len() {
-      let b = buf[n];
-
-      if self.in_string {
-        writer.write(&buf[n..n+1])?;
-        if self.in_backslash { self.in_backslash = false; }
-        else if b == C_QUOTE { self.in_string = false; }
-        else if b == C_BACKSLASH { self.in_backslash = true; }
-      }
-      else {
-        match b {
-          C_SPACE | C_LF | C_CR | C_TAB => {
-            // skip whitespace
-          },
-
-          C_LEFT_BRACKET | C_LEFT_BRACE => {
-            writer.write(&buf[n..n+1])?;
-            self.depth += 1;
-            // don't write trailing whitespace immediately, in case this
-            // is an empty structure
-            self.empty = true;
-          },
-
-          C_RIGHT_BRACKET | C_RIGHT_BRACE => {
-            self.depth -= 1;
-            if self.empty {
-              writer.write(&buf[n..n+1])?;
-              self.empty = false;
+    /// Returns a `Result` containing either a `String` containing
+    /// a formatted `json_string`, or a `String` describing the error.
+    ///
+    /// Example:
+    ///
+    /// ```
+    /// let mut fmt = jsonxf::Formatter::pretty_printer();
+    /// fmt.indent = String::from("\t");
+    /// fmt.trailing_output = String::from("\n");
+    /// assert_eq!(
+    ///     fmt.format("{\"a\":1}").unwrap(),
+    ///     "{\n\t\"a\": 1\n}\n"
+    /// );
+    /// ```
+    pub fn format(&mut self, json_string: &str) -> Result<String, String> {
+        let mut input = json_string.as_bytes();
+        let mut output: Vec<u8> = vec![];
+        match self.format_stream(&mut input, &mut output) {
+            Ok(_) => {}
+            Err(f) => {
+                return Err(f.to_string());
             }
-            else {
-              writer.write(&['\n' as u8])?;
-              for _ in 0..self.depth {
-                writer.write(self.indent.as_bytes())?;
-              }
-              writer.write(&buf[n..n+1])?;
-            }
-            if self.depth == 0 {
-              writer.write(&['\n' as u8])?;
-            }
-          },
-
-          C_COMMA => {
-            writer.write(&[b, '\n' as u8])?;
-            for _ in 0..self.depth {
-              writer.write(self.indent.as_bytes())?;
-            }
-          },
-
-          C_COLON => {
-            writer.write(&[':' as u8, ' ' as u8])?;
-          },
-
-          _ => {
-            if self.empty {
-              writer.write(&[C_LF])?;
-              for _ in 0..self.depth {
-                writer.write(self.indent.as_bytes())?;
-              }
-              self.empty = false;
-            }
-            if b == C_QUOTE {
-              self.in_string = true;
-            }
-            writer.write(&buf[n..n+1])?;
-          },
         };
-      };
-    };
+        let output_string = match String::from_utf8(output) {
+            Ok(s) => s,
+            Err(f) => {
+                return Err(f.to_string());
+            }
+        };
+        return Ok(output_string);
+    }
 
-    return Ok(());
-  }
+    /// Formats the data in `input` and writes it to `output`.
+    ///
+    /// ```no_run
+    /// let mut fmt = jsonxf::Formatter::pretty_printer();
+    /// fmt.indent = String::from("\t");
+    /// fmt.trailing_output = String::from("\n");
+    /// match fmt.format_stream(&mut std::io::stdin(), &mut std::io::stdout()) {
+    ///     Ok(_) => { /* YAY */ },
+    ///     Err(e) => { panic!(e.to_string()); }
+    /// }
+    /// ```
+    pub fn format_stream(&mut self, input: &mut Read, output: &mut Write) -> Result<(), Error> {
+        let mut reader = BufReader::new(input);
+        let mut writer = BufWriter::new(output);
+        let mut buf = [0 as u8; BUF_SIZE];
+        loop {
+            match reader.read(&mut buf) {
+                Ok(0) => {
+                    break;
+                }
+                Ok(n) => {
+                    self.format_buf(&buf[0..n], &mut writer)?;
+                }
+                Err(e) => {
+                    return Err(e);
+                }
+            }
+        }
+        writer.write(self.trailing_output.as_bytes())?;
+        return Ok(());
+    }
+
+
+    /* Formats the contents of `buf` into `writer`. */
+    fn format_buf(&mut self, buf: &[u8], writer: &mut Write) -> Result<(), Error> {
+        for n in 0..buf.len() {
+            let b = buf[n];
+
+            if self.in_string {
+                writer.write(&buf[n..n + 1])?;
+                if self.in_backslash {
+                    self.in_backslash = false;
+                } else if b == C_QUOTE {
+                    self.in_string = false;
+                } else if b == C_BACKSLASH {
+                    self.in_backslash = true;
+                }
+            } else {
+                match b {
+                    C_SPACE | C_LF | C_CR | C_TAB => {
+                        // skip whitespace
+                    }
+
+                    C_LEFT_BRACKET | C_LEFT_BRACE => {
+                        if self.first {
+                            self.first = false;
+                            writer.write(&buf[n..n + 1])?;
+                        } else if self.empty {
+                            writer.write(self.line_separator.as_bytes())?;
+                            for _ in 0..self.depth {
+                                writer.write(self.indent.as_bytes())?;
+                            }
+                            writer.write(&buf[n..n + 1])?;
+
+                        } else if self.depth == 0 {
+                            writer.write(self.record_separator.as_bytes())?;
+                            writer.write(&buf[n..n + 1])?;
+                        } else {
+                            writer.write(&buf[n..n + 1])?;
+                        }
+                        self.depth += 1;
+                        self.empty = true;
+                    }
+
+                    C_RIGHT_BRACKET | C_RIGHT_BRACE => {
+                        self.depth -= 1;
+                        if self.empty {
+                            self.empty = false;
+                            writer.write(&buf[n..n + 1])?;
+                        } else {
+                            writer.write(self.line_separator.as_bytes())?;
+                            for _ in 0..self.depth {
+                                writer.write(self.indent.as_bytes())?;
+                            }
+                            writer.write(&buf[n..n + 1])?;
+                        }
+                    }
+
+                    C_COMMA => {
+                        writer.write(&buf[n..n + 1])?;
+                        writer.write(self.line_separator.as_bytes())?;
+                        for _ in 0..self.depth {
+                            writer.write(self.indent.as_bytes())?;
+                        }
+                    }
+
+                    C_COLON => {
+                        writer.write(&buf[n..n + 1])?;
+                        writer.write(self.after_colon.as_bytes())?;
+                    }
+
+                    _ => {
+                        if self.empty {
+                            writer.write(self.line_separator.as_bytes())?;
+                            for _ in 0..self.depth {
+                                writer.write(self.indent.as_bytes())?;
+                            }
+                            self.empty = false;
+                        }
+                        if b == C_QUOTE {
+                            self.in_string = true;
+                        }
+                        writer.write(&buf[n..n + 1])?;
+                    }
+                };
+            };
+        }
+
+        return Ok(());
+    }
 }
 
 /// Pretty-prints a string of JSON-encoded data.
@@ -195,19 +294,19 @@ impl Formatter {
 ///
 /// ```
 /// assert_eq!(
-///   jsonxf::pretty_print("{\"a\":1,\"b\":2}", "  ").unwrap(),
-///   "{\n  \"a\": 1,\n  \"b\": 2\n}\n"
+///     jsonxf::pretty_print("{\"a\":1,\"b\":2}", "  ").unwrap(),
+///     "{\n  \"a\": 1,\n  \"b\": 2\n}"
 /// );
 /// assert_eq!(
-///   jsonxf::pretty_print("{\"empty\":{},\n\n\n\n\n\"one\":[1]}", "\t").unwrap(),
-///   "{\n\t\"empty\": {},\n\t\"one\": [\n\t\t1\n\t]\n}\n"
+///     jsonxf::pretty_print("{\"empty\":{},\n\n\n\n\n\"one\":[1]}", "\t").unwrap(),
+///     "{\n\t\"empty\": {},\n\t\"one\": [\n\t\t1\n\t]\n}"
 /// );
 /// ```
 ///
 pub fn pretty_print(json_string: &str, indent: &str) -> Result<String, String> {
-  let mut xf = Formatter::pretty_printer();
-  xf.indent = indent.to_owned();
-  return xf.format(json_string);
+    let mut xf = Formatter::pretty_printer();
+    xf.indent = indent.to_owned();
+    return xf.format(json_string);
 }
 
 /// Pretty-prints a stream of JSON-encoded data.
@@ -226,15 +325,15 @@ pub fn pretty_print(json_string: &str, indent: &str) -> Result<String, String> {
 ///
 /// ```no_run
 /// match jsonxf::pretty_print_stream(&mut std::io::stdin(), &mut std::io::stdout(), "\t") {
-///   Ok(_) => { },
-///   Err(e) => { panic!(e.to_string()) }
+///     Ok(_) => { },
+///     Err(e) => { panic!(e.to_string()) }
 /// };
 /// ```
 ///
 pub fn pretty_print_stream(input: &mut Read, output: &mut Write, indent: &str) -> Result<(), Error> {
-  let mut xf = Formatter::pretty_printer();
-  xf.indent = indent.to_owned();
-  return xf.format_stream(input, output);
+    let mut xf = Formatter::pretty_printer();
+    xf.indent = indent.to_owned();
+    return xf.format_stream(input, output);
 }
 
 /// Minimizes a string of JSON-encoded data.
@@ -247,18 +346,17 @@ pub fn pretty_print_stream(input: &mut Read, output: &mut Write, indent: &str) -
 ///
 /// ```
 /// assert_eq!(
-///   jsonxf::minimize("{ \"a\": \"b\", \"c\": 0 } ").unwrap(),
-///   "{\"a\":\"b\",\"c\":0}"
+///     jsonxf::minimize("{ \"a\": \"b\", \"c\": 0 } ").unwrap(),
+///     "{\"a\":\"b\",\"c\":0}"
 /// );
 /// assert_eq!(
-///   jsonxf::minimize("\r\n\tnull\r\n").unwrap(),
-///   "null"
+///     jsonxf::minimize("\r\n\tnull\r\n").unwrap(),
+///     "null"
 /// );
 /// ```
 ///
 pub fn minimize(json_string: &str) -> Result<String, String> {
-  let mut xf = Formatter::minimizer();
-  return xf.format(json_string);
+    Formatter::minimizer().format(json_string)
 }
 
 /// Minimizes a string of JSON-encoded data.
@@ -271,17 +369,15 @@ pub fn minimize(json_string: &str) -> Result<String, String> {
 ///
 /// ```
 /// assert_eq!(
-///   jsonxf::minimize("{ \"a\": \"b\", \"c\": 0 } ").unwrap(),
-///   "{\"a\":\"b\",\"c\":0}"
+///     jsonxf::minimize("{ \"a\": \"b\", \"c\": 0 } ").unwrap(),
+///     "{\"a\":\"b\",\"c\":0}"
 /// );
 /// assert_eq!(
-///   jsonxf::minimize("\r\n\tnull\r\n").unwrap(),
-///   "null"
+///     jsonxf::minimize("\r\n\tnull\r\n").unwrap(),
+///     "null"
 /// );
 /// ```
 ///
 pub fn minimize_stream(input: &mut Read, output: &mut Write) -> Result<(), Error> {
-  let mut xf = Formatter::minimizer();
-  return xf.format_stream(input, output);
+    Formatter::minimizer().format_stream(input, output)
 }
-
--- tests/pretty_print_test.rs
+++ tests/pretty_print_test.rs
@@ -6,47 +6,47 @@ fn pretty_print_passes_test_cases() {
   let test_cases: Vec<[&str; 2]> = vec![
     [ // one object, one k-v pair
       "{\"hello\":\"world\"}",
-      "{\n\t\"hello\": \"world\"\n}\n",
+      "{\n\t\"hello\": \"world\"\n}",
     ],
 
     [ // one object, several k-v pairs
       " { \"hello\": \"world2\",\r\n  \"wow\": \"cool\"  } \r\n",
-      "{\n\t\"hello\": \"world2\",\n\t\"wow\": \"cool\"\n}\n",
+      "{\n\t\"hello\": \"world2\",\n\t\"wow\": \"cool\"\n}",
     ],
 
     [ // simple array
       "[1,2,3]",
-      "[\n\t1,\n\t2,\n\t3\n]\n",
+      "[\n\t1,\n\t2,\n\t3\n]",
     ],
 
     [ // one object per line, with blank lines and missing newlines
       " { \"hello\": \"world3\"}\r\n\n\n  { \"wow\": \"cool\"  }{\"a\":\"b\"}",
-      "{\n\t\"hello\": \"world3\"\n}\n{\n\t\"wow\": \"cool\"\n}\n{\n\t\"a\": \"b\"\n}\n",
+      "{\n\t\"hello\": \"world3\"\n}\n{\n\t\"wow\": \"cool\"\n}\n{\n\t\"a\": \"b\"\n}",
     ],
 
     [ // one array per line, with blank lines and missing newlines
       "[1, 2, \"omg\"]\n\n\n[\"whee\", {}, 22][]",
-      "[\n\t1,\n\t2,\n\t\"omg\"\n]\n[\n\t\"whee\",\n\t{},\n\t22\n]\n[]\n",
+      "[\n\t1,\n\t2,\n\t\"omg\"\n]\n[\n\t\"whee\",\n\t{},\n\t22\n]\n[]",
     ],
 
     [ // nested empty array
       " { \"hello\": [\n\n] }",
-      "{\n\t\"hello\": []\n}\n",
+      "{\n\t\"hello\": []\n}",
     ],
 
     [ // nested empty object
       " { \"hello\": {} }",
-      "{\n\t\"hello\": {}\n}\n",
+      "{\n\t\"hello\": {}\n}",
     ],
 
     [ // nested structures
       " { \"hello\": [ \"world5\" , 22 ] } \r\n",
-      "{\n\t\"hello\": [\n\t\t\"world5\",\n\t\t22\n\t]\n}\n",
+      "{\n\t\"hello\": [\n\t\t\"world5\",\n\t\t22\n\t]\n}",
     ],
 
     [ // empty object special case
       " { \n\r\n\t } ",
-      "{}\n",
+      "{}",
     ],
   ];
 
